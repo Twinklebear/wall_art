29559ae8faa910cdf8cd1ef46a2db0fb8b74a206
Will Usher <willusher.life@gmail.com>
2015-09-03 21:50:09 -0600

    Use QImage to download images from the server
    We now check what the content type of the request is and determine if
    we're downloading some JSON query response or an image we requested.
    Currently we just save the image out but later we'll want to grab the
    blurred and unblurred images and composite them on a background thread.

--- client/src/wall_art_app.cpp
+++ client/src/wall_art_app.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint>
 
 #include <QNetworkRequest>
 #include <QNetworkReply>
@@ -7,6 +8,7 @@
 #include <QJsonObject>
 #include <QJsonArray>
 #include <QJsonDocument>
+#include <QImage>
 
 #include "wall_art_app.h"
 
@@ -25,20 +27,42 @@ void WallArtApp::requestReceived(QNetworkReply *reply){
 	if (reply->error() == QNetworkReply::NoError){
 		int status = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
 		if (status >= 200 && status < 300){
+			// Validate that this is either text (JSON data listing) or an image we're downloading
+			const QVariant content_type_header = reply->header(QNetworkRequest::ContentTypeHeader);
+			if (!content_type_header.isValid() || !content_type_header.canConvert(QMetaType::QString)){
+				std::cout << "Invalid content type header, text or jpeg expected\n";
+				return;
+			}
 			std::cout << "success\n";
-			const QByteArray reply_text = reply->readAll();
-			const QJsonDocument json = QJsonDocument::fromJson(reply_text);
-			if (json.isArray()){
-				const QJsonArray json_array = json.array();
-				if (!json_array.empty()){
-					const QJsonObject json_obj = json_array.at(0).toObject();
-					auto artist_fnd = json_obj.constFind("artist");
-					if (artist_fnd != json_obj.end()){
-						artist_name.setText(artist_fnd->toString());
+			const QString content_type = content_type_header.toString();
+			if (content_type == "image/jpeg"){
+				const QVariant content_length_header = reply->header(QNetworkRequest::ContentLengthHeader);
+				if (!content_length_header.isValid() || !content_length_header.canConvert(QMetaType::Int)){
+					std::cout << "Error: JPEG missing valid content length header\n";
+					return;
+				}
+				int content_len = content_length_header.toInt();
+				const QByteArray reply_image = reply->readAll();
+				const QImage img = QImage::fromData(reinterpret_cast<const uint8_t*>(reply_image.data()), content_len, "JPEG");
+				std::cout << "Downloaded image of dimension " << img.width() << "x" << img.height() << ", saving to downloaded.jpg\n";
+				img.save(QString::fromStdString("downloaded.jpg"), "JPEG");
+				std::cout << "image saved\n";
+			}
+			else {
+				const QByteArray reply_text = reply->readAll();
+				const QJsonDocument json = QJsonDocument::fromJson(reply_text);
+				if (json.isArray()){
+					const QJsonArray json_array = json.array();
+					if (!json_array.empty()){
+						const QJsonObject json_obj = json_array.at(0).toObject();
+						auto artist_fnd = json_obj.constFind("artist");
+						if (artist_fnd != json_obj.end()){
+							artist_name.setText(artist_fnd->toString());
+						}
 					}
 				}
+				std::cout << "Reply: " << json.toJson().toStdString() << "\n";
 			}
-			std::cout << "Reply: " << json.toJson().toStdString() << "\n";
 		}
 		else {
 			std::cout << "Redirect\n";
